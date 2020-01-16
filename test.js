3b07af2214eb005f5dcf17c98ceee2359b5ed232
FatalTsai <Cheney.Tsai@jet-opto.com.tw>
2020-01-16 15:57:27 +0800

    can parse gps data from video

--- test.js
+++ test.js
@@ -1,13 +1,37 @@
 const videoanalyser = require('./video-analyser')
+const fs = require('fs')
 const dvr17 = './1D42C600_243830.MP4'
 const dvr19 = './27D3DC00.MP4'
-//console.log(videoanalyser)
-//console.log(videoanalyser.default.analyseVideo)
+var mainWindow;
+var tmp;
 
-tmp =  videoanalyser.default.analyseVideo(dvr19)
+ videoanalyser.default.analyseVideo(dvr17, async function(err,result){
+                
+        if(err)
+        {
+            console.error(err)
+            return err
+        }
+        //console.log(result)
+        tmp = result
+        fs.writeFileSync("dvr17.json", JSON.stringify(result), 'utf8');
+        return await result 
+    }) 
 
-console.log(tmp)
+setTimeout(function () {
+  console.log(tmp)
+    }, 100)
+
 /*
+ analyseVideo(analyseQueue.pop(), function (err, result) {
+    
+                // Finished so no need to timeout
+                clearTimeout(analyseTimeout);
+
+                processing = false;
+                getMainWindow().webContents.send('analysed-video', result);
+                processQueue();
+            });
 
 module.exports.default =  {
 //export default {
