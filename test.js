0f4fb4b0420dbd3634b907e19e1e382ba9c239c6
FatalTsai <Cheney.Tsai@jet-opto.com.tw>
2020-01-16 10:32:20 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,6 @@
+const videoanalyser = require('./video-analyser')
+
+//console.log(videoanalyser)
+//console.log(videoanalyser.default.analyseVideo)
+
+videoanalyser.default.analyseVideo('./1D42C600_243830.MP4')
